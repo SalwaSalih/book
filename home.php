4f95a4d6fea5b3fcd72b273d0a823b969356fecb
SalwaSalih <143799963+SalwaSalih@users.noreply.github.com>
2023-09-01 14:49:59 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php
+include'header.php';
+?>
+<!doctype html>
+<html>
+<link rel="stylesheet"href="hostyle.css">
+
+<body>
+<br><br><br>
+    <h1 >Welcome to Book's home </h1>
+    <hr>
+    <br><br<br><br<br><br><br><br<br><br<br><br<br><br>
+    
+    <br<br><br<br><br><br><br><br><p class="a"> &nbsp;Books are the plane, and the train<br> &nbsp;and the road, They are the destination&nbsp;<br>&nbsp;and the journey, They are home <br></p>
+    <br><br><br><br><br><br><br><br<br><br<br><br<br><br<br><br><br><br><br<br>
+</html>
+<?php
+include'footer.php';
+?>
